9df4d6529440e6b0c6e45ce73c5f6ef1b4e4559f
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-01-22 15:44:28 +0100

    https certificate

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,8 @@
 	    <meta name="author" content="">
 	    <title>Rubix</title>
 	    <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <link rel="shortcut icon" href="/images/favi/pb.png">
+        <link rel="shortcut icon" href="/images/favi/rubix.png">
+        <link rel="manifest" href="{{url('/manifest.json')}}">
         <link href="/css/loaderone.css" type="text/css" rel="stylesheet">
 
 	    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -25,6 +26,19 @@
            {{-- checks for service worker support.if you have the push manager package then use this line
         if ('serviceWorker' in navigator && 'PushManager' in window) instead of
         if ('serviceWorker' in navigator ) --}}
+        <script>
+            if ('serviceWorker' in navigator ) {
+              window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                      // Registration was successful
+                      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                  }, function(err) {
+                      // registration failed :(
+                      console.log('ServiceWorker registration failed: ', err);
+                  });
+              });
+          }
+          </script>
 	</head>
 	<body>
 	    <div id="root">
